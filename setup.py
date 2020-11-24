ca0ee1b97953585d4879ff936766cf1a0e6a4f8b
Anthony Alberti <aalber@sandia.gov>
2020-11-24 10:15:20 -0700

    llvm version 0.33.0

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         "cx-oracle==7.3.0",
         "dash==0.43.0",
         "joblib==0.16.0",
+        "llvmlite==0.33.0",
         "matplotlib==2.2.5",
         "mock==3.0.5",
         "numba==0.51.1",
